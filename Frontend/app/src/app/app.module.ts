59c5527680cbd466be6786701ee0a5fe6e6162aa
phuoclh <Easybooks>
2023-09-10 16:23:02 +0700

    update

--- Frontend/app/src/app/app.module.ts
+++ Frontend/app/src/app/app.module.ts
@@ -3,12 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FriendComponent } from './friend/friend.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FriendComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule,
